6c328f2f93de16ed521c52fab115914e0bc70097
--unset <--unset>
2023-08-03 00:58:06 +0530

    Minor Refactoring

--- app/src/main/java/com/example/androidreduxsample/redux/Middleware.kt
+++ app/src/main/java/com/example/androidreduxsample/redux/Middleware.kt
@@ -1,13 +1,17 @@
 package com.example.androidreduxsample.redux
 
 import com.example.androidreduxsample.data.MainRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.single
 import javax.inject.Inject
 
 class Middleware @Inject constructor(
-    private val mainRepository: MainRepository
+    private val mainRepository: MainRepository,
+    private val dispatchers: Dispatchers
 ) {
 
     suspend fun handle(action: Action): Flow<Action> = flow {
@@ -25,5 +29,5 @@ class Middleware @Inject constructor(
         }
     }.catch {
         emit(ArticleListAction.Failed(it.message))
-    }
+    }.flowOn(dispatchers.IO)
 }
